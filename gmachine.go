364b2fc6e3815baf503153e9afad7735cf5e0e10
githcorrado <githcorrado@gmail.com>
2021-04-30 09:44:18 +0800

    corrado's code

--- gmachine.go
+++ gmachine.go
@@ -3,4 +3,66 @@ package gmachine
 
 // DefaultMemSize is the number of 64-bit words of memory which will be
 // allocated to a new G-machine by default.
-const DefaultMemSize = 1024
+const (
+	DefaultMemSize = 1024
+	OpHALT uint64 = 0
+	OpNOOP uint64 = 1
+	OpINCA uint64 = 2
+	OpDECA uint64 = 3
+	OpSETA uint64 = 4
+)
+
+type gMachine struct {
+	P uint64
+	A uint64
+	Memory []uint64
+}
+
+func New() *gMachine {
+	gmachine := gMachine{ P: 0, A: 0, Memory: make([]uint64, DefaultMemSize) }
+	return &gmachine
+}
+
+func (g *gMachine) Run() {
+	for {
+		op := g.Memory[g.P]
+		switch op {
+		case OpHALT:
+			g.P += 1
+			return
+		case OpNOOP:
+			g.P += 1
+		case OpINCA:
+			g.A += 1
+			g.P += 1
+		case OpDECA:
+			g.A -= 1
+			g.P += 1
+		case OpSETA:
+			g.P += 1
+			g.A = g.Memory[g.P]
+			g.P += 1
+		}
+	}
+}
+
+func (g *gMachine) ProgramSubtract2From3() {
+	g.Memory[0] = OpSETA
+	g.Memory[1] = 3
+	g.Memory[2] = OpDECA
+	g.Memory[3] = OpDECA
+}
+
+func (g *gMachine) ProgramSubtract2FromX(X uint64) {
+	g.Memory[0] = OpSETA
+	g.Memory[1] = X
+	g.Memory[2] = OpDECA
+	g.Memory[3] = OpDECA
+}
+
+func (g *gMachine) RunProgram(instructions []uint64) {
+	for index, instruction := range instructions {
+		g.Memory[index] = instruction
+	}
+	g.Run()
+}
--- gmachine_test.go
+++ gmachine_test.go
@@ -17,9 +17,112 @@ func TestNew(t *testing.T) {
 	if wantP != g.P {
 		t.Errorf("want initial P value %d, got %d", wantP, g.P)
 	}
+	var wantA uint64 = 0
+	if wantA != g.A {
+		t.Errorf("want initial A value %d, got %d", wantA, g.A)
+	}
 	var wantMemValue uint64 = 0
 	gotMemValue := g.Memory[gmachine.DefaultMemSize-1]
 	if wantMemValue != gotMemValue {
 		t.Errorf("want last memory location to contain %d, got %d", wantMemValue, gotMemValue)
 	}
+}
+
+func TestHalt(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.RunProgram([]uint64{})
+	var want uint64 = 1
+	got := g.P
+	if want != got {
+		t.Errorf("want: %d, got: %d", want, got)
+	}
+}
+
+func TestNOOP(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.RunProgram([]uint64 {
+		gmachine.OpNOOP,
+		gmachine.OpHALT,
+	})
+	var want uint64 = 2
+	got := g.P
+	if want != got {
+		t.Errorf("want: %d, got %d", want, got)
+	}
+}
+
+func TestINCA(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.RunProgram([]uint64{gmachine.OpINCA})
+	var want uint64 = 1
+	got := g.A
+	if want != got {
+		t.Errorf("want %d, got %d", want ,got)
+	}
+}
+
+func TestDECA(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.RunProgram([]uint64{gmachine.OpSETA, 2, gmachine.OpDECA})
+	var want uint64 = 1
+	got := g.A
+	if want != got {
+		t.Errorf("want %d, got %d", want ,got)
+	}
+}
+
+func TestProgramSubtract2From3(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.RunProgram([]uint64{gmachine.OpSETA, 3, gmachine.OpDECA, gmachine.OpDECA})
+	var want uint64 = 1
+	got := g.A
+	if want != got {
+		t.Errorf("want %d, got %d", want ,got)
+	}
+}
+
+func TestSETA(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+	g.RunProgram([]uint64 {gmachine.OpSETA, 5})
+	var want uint64 = 5
+	got := g.A
+	if want != got {
+		t.Errorf("want %d, got %d", want ,got)
+	}
+}
+
+func TestSubtract2(t *testing.T) {
+	t.Parallel()
+	g := gmachine.New()
+
+	g.RunProgram([]uint64{gmachine.OpSETA, 2, gmachine.OpDECA, gmachine.OpDECA})
+	var want2 uint64 = 0
+	got2 := g.A
+	if want2 != got2 {
+		t.Errorf("want: %d got: %d", want2, got2)
+	}
+
+	g.P = 0
+	g.RunProgram([]uint64{gmachine.OpSETA, 10, gmachine.OpDECA, gmachine.OpDECA})
+	var want10 uint64 = 8
+	got10 := g.A
+	if want10 != got10 {
+		t.Errorf("want: %d got: %d", want10, got10)
+	}
+
+	g.P = 0
+	g.RunProgram([]uint64{gmachine.OpSETA, 5, gmachine.OpDECA, gmachine.OpDECA})
+	var want5 uint64 = 3
+	got5 := g.A
+	if want5 != got5 {
+		t.Errorf("want: %d got: %d", want5, got5)
+	}
+
+
 }
